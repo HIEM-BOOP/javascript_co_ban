ae672db4912a11c41642f91dfd71465a938c2b92
hiemkbr <hiemkbr@gmail.com>
2020-08-19 12:49:51 +0700

    cmd

---
+++ Mảng/js/service/mang.js
@@ -0,0 +1,14 @@
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+var likeArray = ['bóng đá','bóng chuyền','bóng bàn','đá cầu'];
+
+
+
+
+
+
+
+
+
+
+
--- Vòng lặp/js/service/vonglap.js
+++ Vòng lặp/js/service/vonglap.js
@@ -21,3 +21,52 @@ function sum(amount){
     }  
     return sum ;  
 }   
+
+function tong(from, to){
+
+    var tong = 0;
+
+    for(var i = from; i < to; i++){
+        tong += i;
+    }
+    return tong ;
+}
+
+function sumOdd(from, to){
+    var sumOdd = 0 ;
+    
+    for(var i = from; i < to; i++){
+        if(i % 2 !== 0){
+            sumOdd += i ;
+        }
+    }
+    return sumOdd;
+}
+
+function listOdds(amount){
+    var sum = [];
+    var dem = 0 ;
+    for(var i = 1; i <= amount; i++){
+        if(i % 2 !== 0){
+            sum.push(i);
+            dem++;          
+        }
+    }
+    return sum;
+}
+
+function listOdd(from,amount){
+    var dem =  1;
+    var list = [];
+    for(from; dem <= amount; from++){
+        if(from % 2 !== 0);
+            list.push(from);    
+            dem ++;
+    }
+    return list;
+}
+
+
+
+
+
