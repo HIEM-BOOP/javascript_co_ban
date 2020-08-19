e3021ef25ff1fead69d0cfd21ca31c0e1317301a
hiemkbr <hiemkbr@gmail.com>
2020-08-19 14:00:51 +0700

    cmd

--- Mảng/js/service/mang.js
+++ Mảng/js/service/mang.js
@@ -2,11 +2,42 @@ var array = [1,2,3,4,5,6,7,8,9,10];
 
 var likeArray = ['bóng đá','bóng chuyền','bóng bàn','đá cầu'];
 
+function getLessThan(n){
+    var list = [];
+    var dem = 0;
+    for(i=0; i<n; i++){
+        list.push(i);
+        dem ++;
+    }
+    return list;
+}
 
+function getOddLessThan(n){
+    var list = [];
+    var dem = 0;
+    for(i=0; i<n; i++){
+        if(i % 2 !== 0){
+            // console.log(i);
+            list.push(i);
+        }
+    }
+    return list;
+} 
 
+function  findFirst(arr){ 
+   
+    var list = [];
 
+    
 
+   return mang ;
+}
 
+console.log(findFirst())
+
+// function findFirst(arr,chuoiCanTim){
+//     var 
+// }
 
 
 
